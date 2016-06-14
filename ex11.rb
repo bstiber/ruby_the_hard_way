586d55bc74fcf5aa6337e8d9a795fd4c2dc0d36a
Bill Stiber <billstiber@gmail.com>
2016-06-14 18:14:13 -0400

    add exercise for ex11

---
+++ ex11.rb
@@ -0,0 +1,7 @@
+p "What is your age?"
+age = gets.chomp
+p "What is your name?"
+name = gets.chomp
+p "What is your height?"
+height = gets.chomp
+puts "you are #{age}, your name is #{name} and you are #{height} tall."
