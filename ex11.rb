7fdb15f91bd846ed896eb36d3484541866f3a4fb
Bill Stiber <billstiber@gmail.com>
2016-06-14 18:21:27 -0400

    add file print deviation

--- ex11.rb
+++ ex11.rb
@@ -5,3 +5,11 @@ name = gets.chomp
 p "What is your height?"
 height = gets.chomp
 puts "you are #{age}, your name is #{name} and you are #{height} tall."
+
+print "What is your age? "
+age = gets.chomp
+print "What is your name? "
+name = gets.chomp
+print "What is your height? "
+height = gets.chomp
+puts "you are #{age}, your name is #{name} and you are #{height} tall."
