fd706a797beb0f8c999f369b9e5131bb8b15a6af
Bill Stiber <billstiber@gmail.com>
2016-06-13 22:47:36 -0400

    add more code to exercise

--- ex10.rb
+++ ex10.rb
@@ -1,2 +1,18 @@
 tabby_cat = "\tI'm tabbed in"
 puts tabby_cat
+persian_cat = "I'm split \non on a line"
+puts persian_cat
+backslash_cat = "I'm \\ a \\ cat"
+puts backslash_cat
+
+fat_cat = """
+I'll do a list:
+\t * cat food
+\t * fishies
+\t * cat nap\n\t * Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
