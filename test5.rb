8fa75ebb00e9ce7468e0564eba4e7c490d22d455
Bill Stiber <billstiber@gmail.com>
2016-06-16 19:47:48 -0400

    add file test5.rb and exercise practice

--- test5.rb
+++ test5.rb
@@ -5,3 +5,11 @@ txt = open(filename)
 puts "Here is your file \'#{filename}\'.\n"
 puts
 print txt.read
+
+puts "type in the name of the file name again"
+filename_again = $stdin.gets.chomp
+
+read_again = open(filename_again)
+puts "this is the name of your file \'#{filename_again}\'"
+puts
+print read_again.read
