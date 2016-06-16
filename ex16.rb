d9295f137f18d50bc2f7e0e45556bf8d8154041b
Bill Stiber <billstiber@gmail.com>
2016-06-15 23:10:29 -0400

    add semi colons and extended text on one line

--- ex16.rb
+++ ex16.rb
@@ -14,11 +14,9 @@ target.truncate(0)
 
 puts "Now I'm going to ask you for three lines."
 
-print "Line 1: "
-line1 = $stdin.gets.chomp
-print "Line 2: "
-line2 = $stdin.gets.chomp
-print "Line 3: "
+print "Line 1: "; line1 = $stdin.gets.chomp
+print "Line 2: "; line2 = $stdin.gets.chomp
+print "Line 3: "; line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 target.write(line1)
