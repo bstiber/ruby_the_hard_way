d51f434c217eadf1d2ad9ea8b7f755c7ad6927d1
Bill Stiber <billstiber@gmail.com>
2016-06-15 20:10:27 -0400

    add file changes to test.rb

--- test.rb
+++ test.rb
@@ -2,3 +2,16 @@ first, second, third = ARGV
 puts "your first variable is #{first}."
 puts "your second variable is #{second}."
 puts "your third variable is #{third}."
+
+puts "enter someting:"
+answer = $stdin.gets.chomp
+
+puts answer
+puts $stdin
+
+
+puts "enter someting:"
+input = $stdin.gets.chomp
+
+puts input
+puts $stdin
