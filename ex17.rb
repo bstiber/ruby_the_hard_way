016c158f926c8076e2e2e69357a4d54b9c5a2312
Bill Stiber <billstiber@gmail.com>
2016-06-16 21:35:58 -0400

    add file changes to ex17.rb and add new_file

--- ex17.rb
+++ ex17.rb
@@ -3,6 +3,20 @@ from_file, to_file = ARGV
 puts "copying #{from_file} to #{to_file}."
 
 in_file = open(from_file)
-in_data = in_file.read
+indata = in_file.read
 
-puts "the input file is #{indata.length} long."
+puts "the input file is #{indata.length} bytes long."
+
+puts "does the output file exist? #{File.exist?(to_file)}"
+puts "Ready hit RETURN to continue, Control-C to exit."
+$stdin.gets.chomp
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Alright, all done!"
+
+out_file.close
+in_file.close
+
+# ruby ex17.rb test.txt new_file.txt
