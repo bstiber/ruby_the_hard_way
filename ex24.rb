64236323dd1963ab70ffadd2abf72f312dae1f60
Bill Stiber <billstiber@gmail.com>
2016-06-18 17:09:02 -0400

    add file changes

--- ex24.rb
+++ ex24.rb
@@ -1,1 +1,19 @@
+puts
 puts "Lets practice everything!"
+puts 'You\'d need to know \'bout escapes with \\ that do \n new lines and \t tabs.'
+puts
+
+# the <<END is a "heredoc". See the Student Questions.
+poem = <<END
+\tthe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and require an explanation
+\n\t\twhere there is none.
+END
+
+puts "---------------"
+puts poem
+puts "---------------"
+puts
