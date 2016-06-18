8d447db333dc9ff121ddc9f6026785dc861bf531
Bill Stiber <billstiber@gmail.com>
2016-06-17 22:13:50 -0400

    add file and code for ex19, test6.rb

--- ex19.rb
+++ ex19.rb
@@ -1,3 +1,5 @@
+
+# defining a method (with two parameters)
 def cheese_and_crackers(cheese_count, box_of_crackers)
   puts "you have #{cheese_count} cheeses."
   puts "you have #{box_of_crackers} boxes of crackers."
@@ -6,16 +8,21 @@ def cheese_and_crackers(cheese_count, box_of_crackers)
 end
 
 puts "we can just give the function numbers directly."
+# passing in integers as arguments
 cheese_and_crackers(20, 30)
 
 puts "or we can use variables from our script"
+# setting variables as integers, to be passed in later as arguments
 amount_of_cheese = 50
 amount_of_crackers = 100
 
+# passing in varibles as arguments
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 puts "we can even do math inside too:"
+# passing in integers with math functions
 cheese_and_crackers(10 + 20, 5 + 6)
 
 puts "and we can combine the two, variables and math:"
+# passing in arguments, variables(representing integers) and integers
 cheese_and_crackers(amount_of_cheese + 120, amount_of_crackers * 1000)
---
+++ test6.rb
@@ -0,0 +1,6 @@
+def cheese(cheese_and_crackers, crackers)
+puts "cheese_and_crackers, #{cheese_and_crackers}."
+puts "crackers, #{crackers}"
+end
+
+cheese(23 +45, 45 - 1)
