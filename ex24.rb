264489c02521d060716873f83b5dfe2cbbf428c7
Bill Stiber <billstiber@gmail.com>
2016-06-18 17:22:20 -0400

    add exercise code additions

--- ex24.rb
+++ ex24.rb
@@ -20,3 +20,20 @@ puts
 
 five = 10 - 2 + (30/4)
 puts "this should be #{five.to_f}."
+
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates
+end
+
+start_point = 1000
+
+beans, jars, crates = secret_formula(start_point)
+
+puts "with a starting point of: #{start_point}"
+puts "we'd have #{beans} beans, #{jars} jars, and #{crates} crates."
+
+start_point = start_point / 10
+puts "we can also do that this way"
