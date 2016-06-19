56ea7dbd2b31990c95182584a8117ab01f1a22d7
Bill Stiber <billstiber@gmail.com>
2016-06-19 17:54:09 -0400

    add file ex31.rb

---
+++ ex31.rb
@@ -0,0 +1,8 @@
+puts "you enter a dark room.  Do you go through door number 1 or door number 2?"
+print "> "
+door = $stdin.gets.chomp
+
+if door == 1
+  puts "there is a giant bear inside eating cheese cake.  What do you do?"
+  puts "1, take the cake."
+  puts "2, scream at the bear."
