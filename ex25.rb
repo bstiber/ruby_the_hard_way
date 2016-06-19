50142eddfa5c669569b809cc253769288fbd0346
Bill Stiber <billstiber@gmail.com>
2016-06-19 12:25:34 -0400

    add file ex25.rb

--- ex25.rb
+++ ex25.rb
@@ -19,7 +19,21 @@ def ex25.print_last_word(words)
   puts word
 end
 
-def e25.sort_sentence(sentence)
+def ex25.sort_sentence(sentence)
   words = ex25.break_words(sentence)
-return ex25.sort_words(words)
+  return ex25.sort_words(words)
+end
+
+def ex25.print_first_and_last(sentence)
+  words = ex25.break_words(sentence)
+  ex25.print_first_word(words)
+  ex25.print_last_word(words)
+end
+
+def print_first_and_last_sorted_sentence(sentence)
+  words = ex25.sort_sentence(sentence)
+  ex25.print_first_word(words)
+  ex25.print_last_word(words)
+end
+
 end
