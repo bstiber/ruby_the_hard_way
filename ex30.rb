d2cdf08c3f8a3bb9756c7c366d2cce263318e65b
Bill Stiber <billstiber@gmail.com>
2016-06-19 16:24:40 -0400

    add code changes

--- ex30.rb
+++ ex30.rb
@@ -1,3 +1,20 @@
 people = 20
 cats = 30
 dogs = 15
+
+if people < cats
+  puts "too many cats, the world is doomed!"
+end
+
+if people > cats
+  puts "Not many cats! The world is saved!"
+end
+
+
+if people < dogs
+  puts "the world is drooled on."
+end
+
+if people > dogs
+  puts "the world is dry"
+end
