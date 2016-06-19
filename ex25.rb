4b9d660266f1e714d26f136534b24bf5c9ac887d
Bill Stiber <billstiber@gmail.com>
2016-06-19 12:31:37 -0400

    add code review and changes

--- ex25.rb
+++ ex25.rb
@@ -1,39 +1,39 @@
-module ex25
+module Ex25
 
-def ex25.break_words(stuff)
+def Ex25.break_words(stuff)
   words = stuff.split(' ')
   return words
 end
 
-def ex25.sort_words(words)
+def Ex25.sort_words(words)
   return words.sort_words
 end
 
-def ex25.print_first_word(words)
+def Ex25.print_first_word(words)
   word = words.shift
   puts word
 end
 
-def ex25.print_last_word(words)
+def Ex25.print_last_word(words)
   word = words.pop
   puts word
 end
 
-def ex25.sort_sentence(sentence)
-  words = ex25.break_words(sentence)
-  return ex25.sort_words(words)
+def Ex25.sort_sentence(sentence)
+  words = Ex25.break_words(sentence)
+  return Ex25.sort_words(words)
 end
 
-def ex25.print_first_and_last(sentence)
-  words = ex25.break_words(sentence)
-  ex25.print_first_word(words)
-  ex25.print_last_word(words)
+def Ex25.print_first_and_last(sentence)
+  words = Ex25.break_words(sentence)
+  Ex25.print_first_word(words)
+  Ex25.print_last_word(words)
 end
 
 def print_first_and_last_sorted_sentence(sentence)
-  words = ex25.sort_sentence(sentence)
-  ex25.print_first_word(words)
-  ex25.print_last_word(words)
+  words = Ex25.sort_sentence(sentence)
+  Ex25.print_first_word(words)
+  Ex25.print_last_word(words)
 end
 
 end
