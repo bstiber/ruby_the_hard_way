aa8ab8a3335d4969eaf42a863020e80e2a1a3af0
Bill Stiber <billstiber@gmail.com>
2016-06-19 21:47:39 -0400

    add file code changes ex33.rb

--- ex33.rb
+++ ex33.rb
@@ -10,3 +10,24 @@ while i < 6
   puts "at the bottom now is #{i}."
 end
 puts "numbers."
+
+
+i = 0
+numbers = []
+
+def loops(numbers)
+  i = 0
+  numbers = []
+
+  while i < 6
+    puts "at the top i is #{i}"
+    numbers << (i)
+
+    i += 1
+    puts "numbers now", numbers, "alphabet-z"
+    puts "at the bottom now is #{i}."
+  end
+  puts "numbers."
+end
+
+loops(5)
