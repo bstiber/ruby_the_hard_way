1b8f92cafca3dd6bd306ba126a785f82f57ecd69
Bill Stiber <billstiber@gmail.com>
2016-06-19 21:28:12 -0400

    add code changes to file ex33.rb

---
+++ ex33.rb
@@ -0,0 +1,12 @@
+i = 0
+numbers = []
+
+while i < 6
+  puts "at the top i is #{i}"
+  numbers << (i)
+
+  i += 1
+  puts "numbers now", numbers, "alphabet-z"
+  puts "at the bottom now is #{i}."
+end
+puts "numbers."
