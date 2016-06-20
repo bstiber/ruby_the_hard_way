3254000533c16f6de9a01859b116cb027b22ea43
Bill Stiber <billstiber@gmail.com>
2016-06-19 21:20:43 -0400

    add code changes and updates

--- ex32.rb
+++ ex32.rb
@@ -1,6 +1,27 @@
 the_count = [1, 2, 3, 4, 5]
 fruits = ['apples', 'oranges', 'banannas', 'pears', 'pineapples']
+change = [1, 'dimes', 2, 'pennies', 3, 'quarters']
 
 for number in the_count
-  puts "this is the count #{the_count}."
+  puts "this is the count #{number}."
 end
+
+the_count.each do |number|
+  puts "\tThis is the count #{number}. "
+end
+
+
+fruits.each do |fruit|
+  puts "A type of: #{fruit}"
+end
+
+change.each {|i| puts "I got #{i}."}
+
+elements = []
+
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  elements.push(i)
+end
+
+elements.each {|i| puts "now we have a list of numbers: #{i+1}"}
