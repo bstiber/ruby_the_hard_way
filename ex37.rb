40c55b059fdb3d16c84ce56dc34f2192da410996
Bill Stiber <billstiber@gmail.com>
2016-06-21 22:38:58 -0400

    add file ex37.rb

---
+++ ex37.rb
@@ -0,0 +1,6 @@
+BEGIN { print "hi this begins.\n"}
+# the END in all caps script runs at the end of everything, its at the END..
+END {print "this is a test testing END in all CAPS\n"}
+puts "hello:" and "goodbye"
+
+# alias => need to research this one?
