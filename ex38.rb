90d3d335e06a81642f0ee86090443dbb7f84d5bd
bstiber <billstiber@gmail.com>
2016-06-24 21:34:28 -0400

    add file ex38.rb

---
+++ ex38.rb
@@ -0,0 +1,5 @@
+ten_things = "apples oranges crows telephone light sugar"
+puts "wait there are not 10 things in that list lets fix that."
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
