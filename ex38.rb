c318de42a385aaca422398498515be2efd0e6b75
bstiber <billstiber@gmail.com>
2016-06-24 21:47:54 -0400

    add code to file

--- ex38.rb
+++ ex38.rb
@@ -3,3 +3,19 @@ puts "wait there are not 10 things in that list lets fix that."
 
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while stuff.length != 10
+  next_one = more_stuff.pop
+  puts "adding #{next_one}."
+  stuff.push(next_one)
+  puts "there are #{stuff.length} items now."
+end
+
+puts "there we go #{stuff}."
+
+puts "lets do some things with stuff"
+puts stuff[1]
+puts stuff[-1]
+puts stuff.pop
+puts stuff.join(' * ')
+puts stuff[3..5].join('#')
