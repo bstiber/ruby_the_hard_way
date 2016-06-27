64bf6af71463c953cf58f81b65169746450cec17
bstiber <billstiber@gmail.com>
2016-06-27 19:24:50 -0400

    add file randomize.rb

---
+++ randomize.rb
@@ -0,0 +1,17 @@
+# randomize
+
+def randomize(number)
+  rand(number) + 1
+end
+
+p randomize(5)
+p randomize(5)
+p randomize(5)
+p randomize(5)
+p randomize(5)
+p randomize(5)
+p randomize(5)
+p randomize(5)
+
+
+# p randomize(10)
